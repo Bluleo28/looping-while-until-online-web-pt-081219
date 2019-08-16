8a7371c414834eba7e23ad2051cae95f3b451783
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-16 18:44:25 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,5 @@
-def using_until
-  levitation_force = 6
-  #your code here
-    
+def using_until 
+  10.until "Wingardium Leviosa"
+levitation_force+1 
+  end
 end
-
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,5 @@
-def using_while
-  levitation_force = 6
-	
-  #your code here
+def using_while 
+  10.while "Wingardium Leviosa"
+levitation_force+1 
+  end
 end
-
-
