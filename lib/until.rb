5519598a1249776c1cde28875eff57447d113406
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-16 18:52:42 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,5 +1,6 @@
-def using_until 
-  10.until "Wingardium Leviosa"
-levitation_force+1 
+def using_until = 6
+until levitation_force == 10 
+puts ""Wingardium Leviosa"
+levitation_force += 1
   end
 end
